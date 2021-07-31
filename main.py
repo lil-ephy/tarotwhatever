33323399cc67f46942226f00a1ac7d9df762e9af
elliewhatever <77340483+elliewhatever@users.noreply.github.com>
2021-07-31 22:03:07 +0100

    big change uwu

--- main.py
+++ main.py
@@ -63,24 +63,6 @@ class spreadPls(object):
         return tarotPls().valuesPls(quantity)
 
 
-def printRaw(quantity):
-    print(*(tarotPls().valuesPls(quantity)))  # PRETTY PRINT UNPACKED LIST
-    print(tarotPls().valuesPls(quantity))  # PRETTY PRINT NESTED LIST
-
-
-def printNice(quantity):
-    for each in tarotPls().valuesPls(quantity):
-        a = "an" if each[0] == "upright" else "a"
-        if each[1] == "minor":
-            print(
-                f"You have drawn {a} [blue]{each[0]}[/blue]: [bold blue]{each[2]}[/bold blue] of [bold blue]{each[3]}[/bold blue]"
-            )
-        else:
-            print(
-                f"You have drawn {a} [blue]{each[0]}[/blue]: [bold blue]{each[3]}[/bold blue] [italic yellow]({toRoman(int(each[2]))})[/italic yellow]"
-            )
-
-
 def readingPls():
     x = f"{datetime.datetime.utcnow():%Y-%m-%d-%T}"
     with open(f'{x.replace(":","")}.txt', "w+") as file:
