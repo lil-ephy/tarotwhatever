fe9e4195c0a88ed6f6439a7dddc096f3e1dda641
elliewhatever <77340483+elliewhatever@users.noreply.github.com>
2021-07-31 20:01:13 +0100

    big change uwu

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 import os
 from roman import toRoman
 from deck import *
+from rich import print as rprint
 
 class tarotPls:
     def __init__(self, marseille=False):
@@ -15,7 +16,7 @@ class tarotPls:
 
     def genPls(self, cards):
         for i in range(cards):
-            x = tarotPls().__dict__
+            x = tarotPls(self.marseille).__dict__
             yield x
     
     def cardPls(self, cards=1):
@@ -23,29 +24,42 @@ class tarotPls:
         for i in self.genPls(cards):
             x = {}
             # print(", ".join(f"{k}: {v}" for k, v in i.items())) # pretty prints original dict
-            x['orientation'] = "Upright" if i['orientation'] else "Reversed"
+            x['orientation'] = "upright" if i['orientation'] else "reversed"
             if i['arcana']:
-                x['card'] = f"{arcanaGen(False)[i['position']]}"
-                x['position'] = f"{toRoman(i['position'])}" if i['position'] > 0 else '0'
+                # x['position'] = f"{toRoman(i['position'])}" if i['position'] > 0 else '0'
+                x['position'] = f"{i['position']}"
+                x['card'] = f"{arcanaGen(self.marseille)[i['position']]}"
             else: 
                 if i['position'] > 9:
-                    x['card'] = f"{cardGen(True)[i['position']-10]} of {cardGen(False)[i.get('suit')]}"
+                    x['position'] = f"{cardGen(True, self.marseille)[i['position']-10]}"
+                    x['suit'] = f"{cardGen(False, self.marseille)[i.get('suit')]}"
                 else:
-                    x['card'] = f"{i['position']} of {cardGen(False)[i.get('suit')]}" if i['position'] != 0 else f"Ace of {cardGen(False)[i.get('suit')]}"
-                    # x['position'] = f"{toRoman(i['position'])}" # add roman numbers to pip cards
+                    # x['position'] = f"{toRoman(i['position'])}" if i['position'] > 0 else "Ace" # add roman numbers to pip cards
+                    x['position'] = f"{i['position']}" if i['position'] > 0 else "Ace"
+                    x['suit'] = f"{cardGen(False, self.marseille)[i.get('suit')]}"
+
             y.append(x)
         return y
+    
+    def valuesPls(self, cards=1):
+        x = []
+        for each in tarotPls(self.marseille).cardPls(cards):
+            x.append(list(v for k,v in each.items()))
+        return x
 
-print(tarotPls().cardPls(1))
+for each in tarotPls(False).valuesPls(10):
+    a = 'an' if each[0] == 'upright' else 'a'
+    # rprint(f"You have drawn {a} [bold magenta]{each[0]}[/bold magenta]: {each[1]}")
+    print(each)
 
 def tests():
+    print('\n################################################################################################################\n')
     # prints cards individually
     for i in tarotPls().cardPls(10):
         print(i)
-
+    # print('\n################################################################################################################\n')
+    # print(tarotPls().cardPls(1))
     print('\n################################################################################################################\n')
-
     # prints cards as list
     print(tarotPls().cardPls(10))
-
-# tests()
+    print('\n################################################################################################################\n')
