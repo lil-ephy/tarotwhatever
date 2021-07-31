cb81beb530225ae8625b25d5f35229ea31c3bd1f
elliewhatever <77340483+elliewhatever@users.noreply.github.com>
2021-07-31 20:36:00 +0100

    big change uwu

--- main.py
+++ main.py
@@ -4,62 +4,83 @@ import os
 from roman import toRoman
 from deck import *
 from rich import print as rprint
+from rich.panel import Panel
 
 class tarotPls:
     def __init__(self, marseille=False):
         random.seed(os.urandom(16))
         self.marseille = marseille
-        self.arcana = bool(random.randint(0,1))
-        self.orientation = bool(random.randint(0,1))
-        self.position = random.randint(0,21) if self.arcana == True else random.randint(0,13)
-        self.suit = None if self.arcana == True else random.randint(0,3)
+        self.arcana = bool(random.randint(0, 1))
+        self.orientation = bool(random.randint(0, 1))
+        self.position = (
+            random.randint(0, 21) if self.arcana == True else random.randint(0, 13)
+        )
+        self.suit = None if self.arcana == True else random.randint(0, 3)
 
     def genPls(self, cards):
         for i in range(cards):
             x = tarotPls(self.marseille).__dict__
             yield x
-    
+
     def cardPls(self, cards=1):
         y = []
         for i in self.genPls(cards):
             x = {}
             # print(", ".join(f"{k}: {v}" for k, v in i.items())) # pretty prints original dict
-            x['orientation'] = "upright" if i['orientation'] else "reversed"
-            if i['arcana']:
+            x["orientation"] = "upright" if i["orientation"] else "reversed"
+            x["arcana"] = "major" if i["arcana"] == True else "minor"
+            if i["arcana"]:
                 # x['position'] = f"{toRoman(i['position'])}" if i['position'] > 0 else '0'
-                x['position'] = f"{i['position']}"
-                x['card'] = f"{arcanaGen(self.marseille)[i['position']]}"
-            else: 
-                if i['position'] > 9:
-                    x['position'] = f"{cardGen(True, self.marseille)[i['position']-10]}"
-                    x['suit'] = f"{cardGen(False, self.marseille)[i.get('suit')]}"
-                else:
+                x["position"] = f"{i['position']}"
+                x["card"] = f"{arcanaGen(self.marseille)[i['position']]}"
+            else:
+                if i["position"] > 9:
+                    x["position"] = f"{cardGen(True, self.marseille)[i['position']-10]}"
+                    x["suit"] = f"{cardGen(False, self.marseille)[i.get('suit')]}"
+                elif 9 > i['position'] > 0:
                     # x['position'] = f"{toRoman(i['position'])}" if i['position'] > 0 else "Ace" # add roman numbers to pip cards
-                    x['position'] = f"{i['position']}" if i['position'] > 0 else "Ace"
-                    x['suit'] = f"{cardGen(False, self.marseille)[i.get('suit')]}"
-
+                    x["position"] = f"{i['position']+1}"
+                    x["suit"] = f"{cardGen(False, self.marseille)[i.get('suit')]}"
+                else:
+                    x['position'] = 'Ace'
+                    x["suit"] = f"{cardGen(False, self.marseille)[i.get('suit')]}"
             y.append(x)
         return y
-    
+
     def valuesPls(self, cards=1):
         x = []
         for each in tarotPls(self.marseille).cardPls(cards):
-            x.append(list(v for k,v in each.items()))
+            x.append(list(v for k, v in each.items()))
         return x
 
-for each in tarotPls(False).valuesPls(10):
-    a = 'an' if each[0] == 'upright' else 'a'
-    # rprint(f"You have drawn {a} [bold magenta]{each[0]}[/bold magenta]: {each[1]}")
-    print(each)
+rprint(*(tarotPls(False).valuesPls(10))) # PRETTY PRINT UNPACKED LIST
+rprint(tarotPls().valuesPls(10)) # PRETTY PRINT NESTED LIST
 
 def tests():
-    print('\n################################################################################################################\n')
+    rprint(
+        "\n################################################################################################################\n"
+    )
     # prints cards individually
     for i in tarotPls().cardPls(10):
-        print(i)
+        rprint(i)
     # print('\n################################################################################################################\n')
     # print(tarotPls().cardPls(1))
-    print('\n################################################################################################################\n')
+    rprint(
+        "\n################################################################################################################\n"
+    )
     # prints cards as list
-    print(tarotPls().cardPls(10))
-    print('\n################################################################################################################\n')
+    rprint(tarotPls().cardPls(10))
+    rprint(
+        "\n################################################################################################################\n"
+    )
+
+    for each in tarotPls().valuesPls(10):
+        a = "an" if each[0] == "upright" else "a"
+        if each[1] == "minor":
+            rprint(
+                f"You have drawn {a} [bold 214]{each[0]}[/bold 214]: [bold magenta]{each[2]}[/bold magenta] of [bold magenta]{each[3]}[/bold magenta]"
+            )
+        else:
+            rprint(
+                f"You have drawn {a} [bold magenta]{each[0]}[/bold magenta]: [bold magenta]{each[3]}[/bold magenta] ({toRoman(int(each[2]))})"
+            )
